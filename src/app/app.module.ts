92c7056c32d653d0fe142d8ea173cf68df6f7033
ZubierAbd <34255944+ZubierAbd@users.noreply.github.com>
2019-07-20 23:17:39 -0400

    Initial Commit - completed title page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { TitleComponent } from "./title/title.component";
 
-import { AppComponent } from './app.component';
-
+const routes: Routes = [
+  { path: "title", component: TitleComponent },
+  { path: "", redirectTo: "/title", pathMatch: "full" }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TitleComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
